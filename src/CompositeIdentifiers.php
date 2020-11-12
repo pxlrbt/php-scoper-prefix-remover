d3e1f7335c2b657492d2d69409e4142676eddb8f
Anton Ukhanev <xedin.unknown@gmail.com>
2020-11-12 08:45:51 +0100

    Add missing docs

--- src/CompositeIdentifiers.php
+++ src/CompositeIdentifiers.php
@@ -2,6 +2,9 @@
 
 namespace pxlrbt\PhpScoper\PrefixRemover;
 
+/**
+ * An identifier collection that aggregates identifiers from multiple other collections.
+ */
 class CompositeIdentifiers implements IdentifiersInterface
 {
     /**
@@ -13,30 +16,44 @@ class CompositeIdentifiers implements IdentifiersInterface
     protected $constants;
     protected $all;
 
-
+    /**
+     * @param iterable<IdentifiersInterface> $identifiersList
+     */
     public function __construct(iterable $identifiersList)
     {
         $this->identifiersList = $identifiersList;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getFunctions(): iterable
     {
         $this->_ensureIndex();
         return $this->functions;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getClasses(): iterable
     {
         $this->_ensureIndex();
         return $this->classes;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getConstants(): iterable
     {
         $this->_ensureIndex();
         return $this->constants;
     }
 
+    /**
+     * @inheritDoc
+     */
     public function getAll(): iterable
     {
         yield from $this->getFunctions();
@@ -44,12 +61,16 @@ class CompositeIdentifiers implements IdentifiersInterface
         yield from $this->getConstants();
     }
 
+    /**
+     * Merges symbols from all internal identifier collections, and caches them.
+     *
+     * The symbols are accessible via the `$functions`, `$classes`, and `$constants` properties afterwards.
+     */
     protected function _ensureIndex()
     {
         if (is_iterable($this->functions) && is_iterable($this->classes) && is_iterable($this->all)) {
             return;
         }
-
 
         foreach ($this->identifiersList as $identifiers) {
             /* @var $identifiers IdentifiersInterface */
@@ -60,6 +81,8 @@ class CompositeIdentifiers implements IdentifiersInterface
     }
 
     /**
+     * Appends a list to the end of another list.
+     *
      * @param iterable $toList A list to append to.
      * @param iterable $sourceList A list to append.
      *
